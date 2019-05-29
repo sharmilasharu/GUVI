c62bc4199035795d9c95bc6ca1039de1793c6146
sharmila <43746533+sharmilasharu@users.noreply.github.com>
2019-05-29 10:56:15 +0530

    Create rev_list.py

---
+++ rev_list.py
@@ -0,0 +1,8 @@
+number=[]
+len_of_dig=int(input ("length of number: "))
+for i in range(len_of_dig):
+  dig_num=int(input("enter the number digits's: "))
+  number.append(dig_num)
+number.reverse()
+print(number)  
+  
