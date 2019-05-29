32f06914ef4632a837bb3a8cee1b214299204234
sharmila <43746533+sharmilasharu@users.noreply.github.com>
2019-05-29 07:25:25 -0700

    Create step number.py

---
+++ step number.py
@@ -0,0 +1,7 @@
+#python code goes here
+#python version :3 
+first number=int(input("enter the first number: "))
+second number=int(input("enter the secod number: "))
+dis=int(input("distence between numbers"))
+for i in range(first number,second number+1,dis):
+    print(i)
